b9a0749054410bbb789a7860a7f70118d9b830af
Constantine Zavezeon <31433211+Kwynto@users.noreply.github.com>
2022-11-19 20:07:41 +0300

    command interface

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,20 +1,23 @@
 package main
 
 import (
+	"bufio"
+	"context"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"sync"
+	"time"
 )
 
-var wg sync.WaitGroup
+// var wg sync.WaitGroup
 
 func serverStart(app *application, srv *http.Server, cfg *Config) {
 	app.infoLog.Printf("Start server: %s", cfg.Addr)
 	err := srv.ListenAndServe()
 	app.errorLog.Println(err)
-	wg.Done()
+	// wg.Done()
 }
 
 func main() {
@@ -42,15 +45,57 @@ func main() {
 	}
 
 	// Server structure with address, logger and routing
-	srv := &http.Server{
-		Addr:     cfg.Addr,
-		ErrorLog: errorLog,
-		Handler:  app.routes(),
+	// srv := &http.Server{
+	// 	Addr:     cfg.Addr,
+	// 	ErrorLog: errorLog,
+	// 	Handler:  app.routes(),
+	// }
+	var srv *http.Server
+
+	time.Sleep(time.Second)
+	fmt.Println("The server is running. You can execute commands.")
+	fmt.Print("> ")
+	sc := bufio.NewScanner(os.Stdin)
+	for sc.Scan() {
+		command := sc.Text()
+		switch command {
+		case "overload":
+			templateCache, err = newTemplateCache("./ui/html/")
+			if err != nil {
+				app.errorLog.Fatal(err)
+			}
+			app.templateCache = templateCache
+			app.infoLog.Println("Templates have been reloaded.")
+		case "stop":
+			// Stoping the server
+			srv.Shutdown(context.Background())
+			app.infoLog.Println("HTTP server Shutdown.")
+		case "start":
+			// Starting the server
+			srv = &http.Server{
+				Addr:     cfg.Addr,
+				ErrorLog: errorLog,
+				Handler:  app.routes(),
+			}
+			go serverStart(app, srv, cfg)
+		case "restart":
+			// ReStarting the server
+			srv.Shutdown(context.Background())
+			app.infoLog.Println("HTTP server Shutdown.")
+			srv = &http.Server{
+				Addr:     cfg.Addr,
+				ErrorLog: errorLog,
+				Handler:  app.routes(),
+			}
+			go serverStart(app, srv, cfg)
+		case "help":
+			fmt.Println("This is helping.")
+		default:
+			fmt.Println("Unidentified command.")
+		}
+		time.Sleep(time.Second)
+		fmt.Print("> ")
 	}
 
-	// Starting the server
-	wg.Add(1)
-	go serverStart(app, srv, cfg)
-
-	wg.Wait()
+	// wg.Wait()
 }
