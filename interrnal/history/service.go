7458c4b20b2d8c29f265ed4e0b9641dcf47c03da
Constantine Zavezeon <31433211+Kwynto@users.noreply.github.com>
2022-12-02 20:31:41 +0300

    dto

--- interrnal/history/service.go
+++ interrnal/history/service.go
@@ -3,6 +3,8 @@ package history
 import "context"
 
 type Service interface {
+	CreateHistory(ctx context.Context, dto CreateHistoryDTO) *History
+
 	GetHistory(ctx context.Context, limit, offset int) []*History
 	GetEntityById(ctx context.Context, id string) *History
 }
@@ -13,6 +15,10 @@ type service struct {
 
 func NewService(storage Storage) Service {
 	return &service{storage: storage}
+}
+
+func (s *service) CreateHistory(ctx context.Context, dto CreateHistoryDTO) *History {
+	return nil
 }
 
 func (s *service) GetHistory(ctx context.Context, limit, offset int) []*History {
